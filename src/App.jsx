014e2b1f8c5c9d019f809037f1808de46cfdc8a8
Avishkar-s73 <avishkar.durgade.s73@kalvium.community>
2025-02-21 17:36:15 +0530

    assignment done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Avishkar Durgade</h1>
+      <p>Email: avishkar@example.com</p>
+      <p>Phone: 1234567890</p>
+    </div>
+  );
+}
+
+export default App;
